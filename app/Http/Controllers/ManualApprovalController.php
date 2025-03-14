1fdede8febff7978a7c4689a679926b2e3ed5b47
BothSann <boththann76@gmail.com>
2025-03-14 12:26:45 +0700

    add models, factories, and seeders for Admin, Manual, Category, Complaint, and ManualApproval

---
+++ app/Http/Controllers/ManualApprovalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ManualApproval;
+use App\Http\Requests\StoreManualApprovalRequest;
+use App\Http\Requests\UpdateManualApprovalRequest;
+
+class ManualApprovalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreManualApprovalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ManualApproval $manualApproval)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ManualApproval $manualApproval)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateManualApprovalRequest $request, ManualApproval $manualApproval)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ManualApproval $manualApproval)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ManualController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Manual;
+use App\Http\Requests\StoreManualRequest;
+use App\Http\Requests\UpdateManualRequest;
+
+class ManualController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreManualRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Manual $manual)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Manual $manual)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateManualRequest $request, Manual $manual)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Manual $manual)
+    {
+        //
+    }
+}
