5e0f4086785f34c289baebbd4877abf8ccb4b561
BothSann <boththann76@gmail.com>
2025-03-17 19:50:56 +0700

    add routes for manuals and complaints with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\Frontend\FrontendController;
+use App\Http\Controllers\ManualController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,16 +10,36 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // })->middleware('custom.guest:web,admin');
 
-Route::get('/', [FrontendController::class,'index'])->name('homepage');
+Route::get('/', [FrontendController::class,'index'])->name('homepage')->middleware('custom.guest:web,admin');
 
 Route::get('/dashboard', [FrontendController::class,'index'])
     ->middleware(['auth:web', 'verified'])->name('dashboard');
 
-Route::middleware('auth:web')->group(function () {
+Route::middleware(['auth:web', 'verified', 'check.banned'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    // Manual Route
+    Route::get('manuals/mymanuals', [ManualController::class, 'indexV2'])->name('manuals.indexv2');
+    Route::get('/manuals/{manual}/download', [ManualController::class, 'download'])->name('manuals.download');
+    Route::get('/manuals/search', [ManualController::class, 'search'])->name('manuals.search');
+    Route::resource('manuals', ManualController::class);
+
+    // Complaint Route
+    Route::resource('complaints', ComplaintController::class)->middleware('auth:web');
 });
+
+
+// Route::resource('manuals', ManualController::class)->middleware('auth:web');
+// Route::get('manuals/mymanuals', [ManualController::class, 'indexV2'])
+//     ->name('manuals.indexv2')
+//     ->middleware('auth:web');
+
+
+
+
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
