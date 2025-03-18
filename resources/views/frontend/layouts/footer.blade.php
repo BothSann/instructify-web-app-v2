3ae3b3cc2edba34a00d0122c383e8a10d7d8793f
BothSann <boththann76@gmail.com>
2025-03-18 21:38:06 +0700

    fix: update footer to display current year dynamically and enhance attribution text

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -2,7 +2,13 @@
 <footer class="mt-12 bg-white border-t border-gray-200">
     <div class="px-4 py-12 mx-auto overflow-hidden max-w-7xl sm:px-6 lg:px-8">
         <p class="text-base text-center text-gray-400 ">
-            &copy; 2025 Instructify Library. All rights reserved.
+            &copy; <span id="year"></span> | Made with 💖 by Both Sann | All rights reserved.
         </p>
     </div>
 </footer>
+
+@push('scripts')
+    <script>
+        document.getElementById("year").textContent = new Date().getFullYear();
+    </script>
+@endpush
